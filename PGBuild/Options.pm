3248b5144d1e1bed3fcb6daea7aa35399594e52d
Andrew Dunstan <andrew@dunslane.net>
2010-11-12 17:43:10 +0000

    Add PGBuild/Options module forgotten in the last commit (thanks to Alvaro Herrera for noticing). Update Makefile.

---
+++ PGBuild/Options.pm
@@ -0,0 +1,75 @@
+
+package PGBuild::Options;
+
+# common options code for buildfarm scripts, so it stays in sync
+
+
+use strict;
+use warnings;
+use Getopt::Long;
+
+use vars qw(@option_list);
+
+BEGIN
+{
+	@option_list = 
+	  qw(
+			$forcerun $buildconf $keepall $ipcclean $help $multiroot
+			$quiet $from_source $from_source_clean $testmode
+			$test_mode $skip_steps $find_typedefs 
+			$nosend $nostatus $verbose
+	   );
+}
+
+use Exporter   ();
+our (@ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
+
+use vars qw($VERSION); $VERSION = 'REL_0.0';
+
+@ISA         = qw(Exporter);
+@EXPORT      = @option_list;
+%EXPORT_TAGS = ( );
+@EXPORT_OK   = ( );
+
+our (
+	 $forcerun, $buildconf, $keepall, $ipcclean, $help, $multiroot,
+	 $quiet, $from_source, $from_source_clean, $testmode,
+	 $test_mode, $skip_steps, $find_typedefs,
+	 $nosend, $nostatus, $verbose,
+	);
+
+use vars qw (%standard_options);
+
+%standard_options =
+  ('nosend' => \$nosend, 
+   'config=s' => \$buildconf,
+   'from-source=s' => \$from_source,
+   'from-source-clean=s' => \$from_source_clean,
+   'force' => \$forcerun,
+   'find-typedefs' => \$find_typedefs,
+   'keepall' => \$keepall,
+   'ipcclean' => \$ipcclean,
+   'verbose:i' => \$verbose,
+   'nostatus' => \$nostatus,
+   'test' => \$testmode,
+   'help' => \$help,
+   'quiet' => \$quiet,
+   'skip-steps=s' => \$skip_steps,
+   'multiroot' => \$multiroot,
+);
+
+$buildconf = "build-farm.conf"; # default value
+
+# extra options can be used by a wrapper program, such as 
+# the one that will do the global lock and election, and it will
+# still have acces to what it needs to do to invoke run_build.
+
+sub fetch_options
+{
+	GetOptions(%standard_options, @_)
+	  || die "bad command line";
+	
+}
+
+
+1;
