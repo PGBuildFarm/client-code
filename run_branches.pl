2134383ca99acbe62e630f6d97ab612a5013f836
Andrew Dunstan <andrew@dunslane.net>
2011-07-28 10:53:45 -0400

    Explicitly use perl from the path to run build script.
    (and not this perl, so don't use $^X)
    This script needs to run on Cygwin with non-cygwin perl if it's running
    in tandem with AS/MinGW perl, since Cygwin perl doesn't honor locks
    the samne way, and the global lock fails. But the build script needs
    to run with the native perl, even on Cygwin, which it picks up from
    the path. (Head exploding yet?).

--- run_branches.pl
+++ run_branches.pl
@@ -143,7 +143,14 @@ sub run_branch
 {
     my $branch = shift;
     my @args = ($run_build,PGBuild::Options::standard_option_list(), $branch);
-    system(@args);
+
+	# Explicitly use perl from the path (and not this perl, so don't use $^X)
+	# This script needs to run on Cygwin with non-cygwin perl if it's running
+	# in tandem with AS/MinGW perl, since Cygwin perl doesn't honor locks
+	# the samne way, and the global lock fails. But the build script needs
+	# to run with the native perl, even on Cygwin, which it picks up from
+	# the path. (Head exploding yet?). 
+    system("perl",@args);
 }
 
 sub branch_last_sort
