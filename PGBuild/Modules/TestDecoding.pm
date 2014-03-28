69c92f53bbe3748c13fa29aee0c39c8dd7210f1e
Andrew Dunstan <andrew@dunslane.net>
2014-03-27 20:09:21 -0400

    Add a module to run the new contrib test_decoding module.
    It can't run installcheck because it requires a special wal_level.
    
    Still some work to do, but it runs on Unix.

---
+++ PGBuild/Modules/TestDecoding.pm
@@ -0,0 +1,92 @@
+
+package PGBuild::Modules::TestDecoding;
+
+use PGBuild::Options;
+use PGBuild::SCM;
+
+use strict;
+
+use vars qw($VERSION); $VERSION = 'REL_4.12';
+
+my $hooks = {
+    'check' => \&check,
+};
+
+sub setup
+{
+    my $class = __PACKAGE__;
+
+    my $buildroot = shift; # where we're building
+    my $branch = shift; # The branch of Postgres that's being built.
+    my $conf = shift;  # ref to the whole config object
+    my $pgsql = shift; # postgres build dir
+
+	# for now do nothing on MSVC
+	return if $conf->{using_msvc};
+
+	# only for supported branches
+	return unless $branch eq 'HEAD' || $branch ge 'REL9_4_STABLE';
+
+    my $self  = {
+        buildroot => $buildroot,
+        pgbranch=> $branch,
+        bfconf => $conf,
+        pgsql => $pgsql
+    };
+    bless($self, $class);
+
+    main::register_module_hooks($self,$hooks);
+
+}
+
+sub check
+{
+    my $self = shift;
+
+    return unless main::step_wanted('test-decoding-check');
+
+    print main::time_str(), "checking test-decoding\n" if	$verbose;
+
+    my $make = $self->{bfconf}->{make};
+
+    my @checklog;
+
+    if ($self->{bfconf}->{using_msvc})
+    {
+#        chdir "$self->{pgsql}/src/tools/msvc";
+#        @checklog = `perl vcregress.pl upgradecheck 2>&1`;
+#        chdir "$self->{buildroot}/$self->{pgbranch}";
+    }
+    else
+    {
+        my $cmd = "cd $self->{pgsql}/contrib/test_decoding && $make check";
+        @checklog = `$cmd 2>&1`;
+    }
+
+    my @logfiles = glob("$self->{pgsql}/contrib/test_decoding/*.log");
+    foreach my $log (@logfiles)
+    {
+        my $fname = basename $log;
+        local $/ = undef;
+        my $handle;
+        open($handle,$log);
+        my $contents = <$handle>;
+        close($handle);
+        push(@checklog,
+            "=========================== $fname ================\n",$contents);
+    }
+
+    my $status = $? >>8;
+
+    main::writelog("test-decoding-check",\@checklog);
+    print "======== test-decoding check log ===========\n",@checklog
+      if ($verbose > 1);
+    main::send_result("test-decoding-check",$status,\@checklog) if $status;
+    {
+        no warnings 'once';
+        $main::steps_completed .= " test-decoding-check";
+    }
+	
+}
+
+1;
