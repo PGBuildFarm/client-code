PGBuildFarm/client-code

commit ee774d22660dc8d23010c1d2ddeb025e12b7d285

Clone using Data::Dumper instead of Storable.
The MSys DTK perl lacks Storable. I keep forgetting that.

run_build.pl
@@ -70,7 +70,6 @@ use POSIX qw(:signal_h strftime);
 use Data::Dumper;
 use Cwd qw(abs_path getcwd);
 use File::Find ();
-use Storable qw(dclone);
 
 use PGBuild::SCM;
 use PGBuild::Options;
@@ -443,9 +442,12 @@ END
 
 # Prepend the DEFAULT settings (if any) to any settings for the
 # branch. Since we're mangling this, deep clone $extra_config
-# so the config object is kept as given.
+# so the config object is kept as given. This is done using
+# Dumper() because the MSys DTK perl doesn't have Storable. This
+# is less efficient but it hardly matters here for this shallow
+# structure.
 
-$extra_config = dclone($extra_config);
+$extra_config = eval Dumper($extra_config);
 
 if ($extra_config &&  $extra_config->{DEFAULT})
 {

commit 9eb9e6cb532d7d3f1cbb73e1bccff2323dd93579

Fix bug in cloning extra_config.
The bug was introduced in commit ee774d22.

Per gripe from Tom Lane.

run_build.pl
@@ -465,7 +465,7 @@ END
 # is less efficient but it hardly matters here for this shallow
 # structure.
 
-$extra_config = eval Dumper($extra_config);
+eval Data::Dumper->Dump([$extra_config],['extra_config']);
 
 if ($extra_config &&  $extra_config->{DEFAULT})
 {