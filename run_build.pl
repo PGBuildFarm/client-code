a398bdfbc2c32dd850a5f09d4f3f589a403c4168
Andrew Dunstan <andrew@dunslane.net>
2015-11-18 22:40:01 -0500

    re-enable tap tests on msys where branch > 9.5

--- run_build.pl
+++ run_build.pl
@@ -1544,9 +1544,10 @@ sub make_bin_installcheck
     # tests only came in with 9.4
     return unless ($branch eq 'HEAD' or $branch ge 'REL9_4');
 
-    # Msys perl crashes and burns on IPC::Run, and we can't use AS perl
-    # because it doesn't handle the virtua; paths. so don't even try.
-    return if ($^O eq 'msys');
+	if ($^O eq 'msys' || $using_msvc)
+	{
+		return unless $branch eq 'HEAD' || $branch gt 'REL9_6';
+	}
 
     # don't run unless the tests have been enabled
     if ($using_msvc)
@@ -1559,6 +1560,15 @@ sub make_bin_installcheck
     }
 
     print time_str(),"running make bin installcheck ...\n" if $verbose;
+
+
+    # fix path temporarily on msys
+    my $save_path = $ENV{PATH};
+    if ($^O eq 'msys')
+    {
+        my $perlpathdir = dirname($Config{perlpath});
+        $ENV{PATH} = "$perlpathdir:$ENV{PATH}";
+    }
 
     my @makeout;
 
@@ -1592,6 +1602,10 @@ sub make_bin_installcheck
     writelog('bin-check',\@makeout);
     print "======== make bin-install-check log ===========\n",@makeout
       if ($verbose > 1);
+
+    # restore path
+    $ENV{PATH} = $save_path;
+
 
     send_result('BinInstallCheck',$status,\@makeout) if $status;
     $steps_completed .= " BinInstallCheck";
