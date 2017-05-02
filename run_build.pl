PGBuildFarm/client-code

commit 9c68e7e4ef39fa226b6ce708a463b740f56d1e33

Handle a corner case where there is no config.log
If configure has a syntax error there might be no config.log at all, and
this causes the web transaction to fail. In this case just report the
error and the rest of the summary.

run_build.pl
@@ -2143,6 +2143,11 @@ sub get_config_summary
     my $config = "";
     unless ($using_msvc)
     {
+		# if configure bugs out there might not be a log file at all
+		# in that case just return the rest of the summary.
+
+		last unless -e "$pgsql/config.log";
+
         open($handle,"$pgsql/config.log") || return undef;
         my $start = undef;
         while (<$handle>)

commit 9347b10554e2975de1e509653a2060c3e13ffe72

Fix perl bug introduced in commit 9c68e7e4
You can't do a "last" exit from an "unless" block. Not sure why when you
can from a bare block, but there it is.

run_build.pl
@@ -2286,13 +2286,12 @@ sub get_config_summary
 {
     my $handle;
     my $config = "";
-    unless ($using_msvc)
+
+	# if configure bugs out there might not be a log file at all
+	# in that case just return the rest of the summary.
+
+    unless ($using_msvc || ! -e "$pgsql/config.log" )
     {
-        # if configure bugs out there might not be a log file at all
-        # in that case just return the rest of the summary.
-
-        last unless -e "$pgsql/config.log";
-
         open($handle,"$pgsql/config.log") || return undef;
         my $start = undef;
         while (<$handle>)