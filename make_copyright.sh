6a3f18680de352e8c9fd95b4c623f43db871a018
Andrew Dunstan <andrew@dunslane.net>
2021-01-15 16:06:59 -0500

    fix copyright script

--- make_copyright.sh
+++ make_copyright.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
-
-make show | while read f ; do
+for f in `make -s show`
+do
 	grep -q Copyright $f || sed -i '3r copyright.template' $f
 done
 
 DT=`date +%Y`
 
-make show | while read f ; do
+for f in `make -s show`
+do
 	sed -i "s/2003-20.., Andrew Dunstan/2003-$DT, Andrew Dunstan/" $f
 done
