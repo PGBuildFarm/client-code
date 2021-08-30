PGBuildFarm/client-code

commit 6810f2ed12d595e2c2ffd00aa94a4fd30c6cd623

allow --no-keepall

PGBuild/Options.pm
@@ -58,7 +58,7 @@ my (%standard_options);
 	'from-source-clean=s' => \$from_source_clean,
 	'force'               => \$forcerun,
 	'find-typedefs'       => \$find_typedefs,
-	'keepall'             => \$keepall,
+	'keepall!'            => \$keepall,          # allow --no-keepall
 	'verbose:i'           => \$verbose,
 	'nostatus'            => \$nostatus,
 	'test'                => \$testmode,

commit 173f0f2eec98a014556afd9c73df12d77e2254d6

fix bug introduced by commit 6810f2e

PGBuild/Options.pm
@@ -99,7 +99,7 @@ sub standard_option_list
 	my @result = ();
 	foreach my $k (keys %standard_options)
 	{
-		(my $nicekey = $k) =~ s/[=:].*//;
+		(my $nicekey = $k) =~ s/[=:!].*//;
 		my $vref = $standard_options{$k};
 		$vref = \$orig_verbose if $nicekey eq 'verbose';
 		next