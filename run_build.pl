9ea92d227a597e0320482e1fea5da7424cdac9f3
Andrew Dunstan <andrew@dunslane.net>
2023-02-25 09:53:23 -0500

    honor use_installcheck_parallel for msvc

--- run_build.pl
+++ run_build.pl
@@ -1651,8 +1651,9 @@ sub make_install_check
 	}
 	else
 	{
+		my $parallel = $use_installcheck_parallel ? "parallel" : "";
 		chdir "$pgsql/src/tools/msvc";
-		@checklog = run_log("perl vcregress.pl installcheck");
+		@checklog = run_log("perl vcregress.pl installcheck $parallel");
 		chdir $branch_root;
 	}
 	my $status   = $? >> 8;
