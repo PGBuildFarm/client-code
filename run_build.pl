0c4b5823c044f5421bf3c81ac421c16d47af810f
Andrew Dunstan <andrew@dunslane.net>
2023-04-19 11:01:10 -0400

    Revert "tighten up TAP arget, find more log files"
    This reverts commit cfbb2b428bb5c40b7809cafd12915e21ef196420.
    
    This was ill-considered.

--- run_build.pl
+++ run_build.pl
@@ -2069,7 +2069,7 @@ sub make_misc_check
 		my @out = run_log("cd $dir && $make $instflags TAP_TESTS= check");
 		$status ||= $? >> 8;
 		push(@checklog, "=========== Module $test check =============\n", @out);
-		my @logs = glob("$dir/regression.diffs $dir/*/regression.diffs $dir/log/*.log");
+		my @logs = glob("$dir/regression.diffs $dir/log/*.log");
 		$log->add_log($_) foreach (@logs);
 	}
 	push(@checklog, $log->log_string);
@@ -2212,7 +2212,6 @@ sub run_tap_test
 	my $is_install_check = shift;
 
 	my $taptarget = $is_install_check ? "installcheck" : "check";
-	my $targname  = "prove_$taptarget";
 
 	return unless step_wanted("$testname-$taptarget");
 
@@ -2249,7 +2248,7 @@ sub run_tap_test
 		my $instflags = $temp_inst_ok ? "NO_TEMP_INSTALL=yes" : "";
 
 		@makeout =
-		  run_log("cd $dir && $make NO_LOCALE=1 $pflags $instflags $targname");
+		  run_log("cd $dir && $make NO_LOCALE=1 $pflags $instflags $taptarget");
 	}
 
 	my $status = $? >> 8;
