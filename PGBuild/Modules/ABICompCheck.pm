d04d1c2a4344908ceff12bc1ba1b8f58d4c5d716
Mankirat Singh <msingh2_be22@thapar.edu>
2025-06-21 01:45:19 +0530

    add abidiff comparing between pairs of commits

--- PGBuild/Modules/ABICompCheck.pm
+++ PGBuild/Modules/ABICompCheck.pm
@@ -1,4 +1,3 @@
-
 # Package Namespace is hardcoded. Modules must live in
 # PGBuild::Modules
 
@@ -53,6 +52,19 @@ sub setup
 		$abi_compare_root = "$buildroot/abicheck";
 	}
 
+	my %binaries_rel_path = $conf->{binaries_rel_path} || (
+		'postgres' => 'bin/postgres',
+		'ecpg' => 'bin/ecpg',
+		'libpq.so' => 'lib/libpq.so',
+	);
+
+	my @abidw_flags_list = $conf->{abidw_flags_list} || (
+		'--drop-undefined-syms', '--no-architecture',
+		'--no-comp-dir-path', '--no-elf-needed',
+		'--no-show-locs', '--type-id-style',
+		'hash',
+	);
+
 	mkdir $abi_compare_root
 	  unless -d $abi_compare_root;
 	mkdir "$abi_compare_root/logs"
@@ -61,6 +73,8 @@ sub setup
 	  unless -d "$abi_compare_root/xmls";
 	mkdir "$abi_compare_root/install"
 	  unless -d "$abi_compare_root/install";
+	mkdir "$abi_compare_root/diffs"
+	  unless -d "$abi_compare_root/diffs";
 
 	my $scm = PGBuild::SCM->new(\%PGBuild::conf);
 	my $self = {
@@ -70,6 +84,8 @@ sub setup
 		pgsql => $pgsql,
 		abi_compare_root => $abi_compare_root,
 		scm => $scm,
+		binaries_rel_path => \%binaries_rel_path,
+		abidw_flags_list => \@abidw_flags_list,
 	};
 	bless($self, $class);
 
@@ -104,6 +120,8 @@ sub need_run
 
 		my $head_commit_hash =
 		  file_contents("$buildroot/$branch/$animal.lastrun-logs/githead.log");
+
+		chomp $head_commit_hash if defined $head_commit_hash;
 
 		$self->{last_commit_hash} = $last_commit_hash;
 		$self->{head_commit_hash} = $head_commit_hash;
@@ -164,10 +182,7 @@ sub _checkout_commit
 
 sub _log_command_output
 {
-	my ($self, $cmd, $commit_hash, $cmd_desc) = @_;
-
-	my $abi_compare_root = $self->{abi_compare_root};
-	my $log_dir = "$abi_compare_root/logs/$commit_hash";
+	my ($self, $cmd, $log_dir, $cmd_desc) = @_;
 
 	# Ensure log directory exists
 	mkdir $log_dir unless -d $log_dir;
@@ -176,14 +191,8 @@ sub _log_command_output
 
 	print time_str(), "Executing: $cmd_desc\n" if $verbose;
 
-	my @output = run_log($cmd);
+	run_log(qq{$cmd > "$log_file" 2>&1});
 	my $exit_status = $? >> 8;
-
-	open my $fh, '>', $log_file
-	  or die "Cannot open log file $log_file for writing: $!";
-	print $fh "$_\n" for @output;
-	close $fh;
-
 	if ($exit_status)
 	{
 		die "$cmd_desc failed with status $exit_status. Log: $log_file";
@@ -200,6 +209,7 @@ sub _configure_make_and_build
 {
 	my ($self, $commit_hash) = @_;    # Added $commit_hash argument
 	my $abi_compare_root = $self->{abi_compare_root};
+	my $log_dir = "$abi_compare_root/logs/$commit_hash";
 
 	print time_str(),
 	  "Configuring, making, and installing for commit $commit_hash in ",
@@ -212,13 +222,11 @@ sub _configure_make_and_build
 
 	_log_command_output($self,
 		qq{./configure CFLAGS="-Og -g" --prefix=$abi_compare_root/install/},
-		$commit_hash, 'configure');
-	print time_str(), "Configuration complete for commit $commit_hash.\n"
-	  if $verbose;
+		$log_dir, 'configure');
 
-	_log_command_output($self, qq{make}, $commit_hash, 'make');
+	_log_command_output($self, qq{make}, $log_dir, 'make');
 
-	_log_command_output($self, q{make install}, $commit_hash, 'makeinstall');
+	_log_command_output($self, q{make install}, $log_dir, 'makeinstall');
 
 	return;
 }
@@ -226,13 +234,8 @@ sub _configure_make_and_build
 sub _generate_abidw_xml
 {
 	my $self = shift;
-	my @abidw_flags_list = (
-		'--drop-undefined-syms', '--no-architecture',
-		'--no-comp-dir-path', '--no-elf-needed',
-		'--no-show-locs', '--type-id-style',
-		'hash',
-	);
-	my $abidw_flags_str = join(' ', @abidw_flags_list);
+	my $abidw_flags_list = $self->{abidw_flags_list};
+	my $abidw_flags_str = join(' ', @$abidw_flags_list);
 	my $commit_hash = shift;
 
 	print time_str(), "Generating ABIDW XML for commit $commit_hash in ",
@@ -240,6 +243,7 @@ sub _generate_abidw_xml
 	  if $verbose;
 
 	my $abi_compare_root = $self->{abi_compare_root};
+	my $binaries_rel_path = $self->{binaries_rel_path};
 	my $commit_xml_dir = "$abi_compare_root/xmls/$commit_hash";
 
 	mkdir $commit_xml_dir unless -d $commit_xml_dir;
@@ -260,23 +264,33 @@ sub _generate_abidw_xml
 		  "Error: Directory $install_bin_dir is empty. Cannot generate ABI XML for commit $commit_hash.";
 	}
 
-	my @targets_to_process = (
+	my @targets_to_process = ();
+	foreach my $target_name (keys %$binaries_rel_path)
+	{
+		my $input_path = "$install_dir/$binaries_rel_path->{$target_name}";
+		my $output_file = "$commit_xml_dir/$target_name.abi";
+
+		if (-e $input_path)
 		{
-			name => 'postgres',
-			input_path => "$install_bin_dir/postgres",
-			output_file => "$commit_xml_dir/postgres.abi",
-		},
-		{
-			name => 'ecpg',
-			input_path => "$install_bin_dir/ecpg",
-			output_file => "$commit_xml_dir/ecpg.abi",
-		},
-		{
-			name => 'libpq.so',
-			input_path => "$install_lib_dir/libpq.so",
-			output_file => "$commit_xml_dir/libpq.so.abi",
+			push @targets_to_process,
+			  {
+				name => $target_name,
+				input_path => $input_path,
+				output_file => $output_file,
+			  };
 		}
-	);
+		else
+		{
+			print time_str(),
+			  "Warning: Input file '$input_path' for $target_name not found. Skipping ABI generation for this target (commit $commit_hash).\n"
+			  if $verbose;
+		}
+	}
+
+	if (!@targets_to_process)
+	{
+		die "No valid targets found for ABI generation in commit $commit_hash.";
+	}
 
 	foreach my $target (@targets_to_process)
 	{
@@ -284,10 +298,10 @@ sub _generate_abidw_xml
 		my $input_file_path = $target->{input_path};
 		my $output_abi_file = $target->{output_file};
 
-		unless (-e $input_file_path)
+		unless (-e $input_file_path && -f $input_file_path)
 		{
 			print time_str(),
-			  "Warning: Input file '$input_file_path' for $target_name not found. Skipping ABI generation for this target (commit $commit_hash).\n"
+			  "Warning: Input file '$input_file_path' for $target_name not found or is not a regular file. Skipping ABI generation for this target (commit $commit_hash).\n"
 			  if $verbose;
 			next;
 		}
@@ -316,6 +330,41 @@ sub _generate_abidw_xml
 	return;
 }
 
+sub _compare_abixml_using_abidiff
+{
+	my ($self, $old_commit_hash, $new_commit_hash) = @_;
+	my $abi_compare_root = $self->{abi_compare_root};
+
+	print time_str(),
+	  "Comparing ABI XML for commits $old_commit_hash and $new_commit_hash in ",
+	  __PACKAGE__, "\n"
+	  if $verbose;
+	my $old_commit_xml_dir = "$abi_compare_root/xmls/$old_commit_hash";
+	my $new_commit_xml_dir = "$abi_compare_root/xmls/$new_commit_hash";
+
+	foreach my $key (keys %{ $self->{binaries_rel_path} })
+	{
+		my $old_file = "$old_commit_xml_dir/$key.abi";
+		my $new_file = "$new_commit_xml_dir/$key.abi";
+
+		if (-e $old_file && -e $new_file)
+		{
+			my $log_dir = "$abi_compare_root/diffs";
+			_log_command_output(
+				$self, "abidiff $old_file $new_file",
+				$log_dir, "$key-$old_commit_hash-$new_commit_hash"
+			);
+		}
+		else
+		{
+			print time_str(),
+			  "Warning: One of the ABI XML files for $key does not exist. Skipping comparison.\n"
+			  if $verbose;
+		}
+	}
+	return;
+}
+
 sub _process_commits_list
 {
 	my $self = shift;
@@ -325,11 +374,20 @@ sub _process_commits_list
 	print time_str(), "Processing commits list in ", __PACKAGE__, "\n"
 	  if $verbose;
 
+	my $prev_commit;
+	my $idx = 0;
 	foreach my $commit (@$commits_list)
 	{
 		_checkout_commit($self, $commit);
 		_configure_make_and_build($self, $commit);
 		_generate_abidw_xml($self, $commit);
+
+		if (defined $prev_commit)
+		{
+			_compare_abixml_using_abidiff($self, $prev_commit, $commit);
+		}
+		$prev_commit = $commit;
+		$idx++;
 	}
 
 	return;
@@ -337,15 +395,15 @@ sub _process_commits_list
 
 sub print_logs
 {
-	my ($clonelog) = @_;
+	my ($logss) = @_;
 
 	# Print the git clone log if it exists
-	if (@$clonelog)
+	if (@$logss)
 	{
-		print "ABICompCheck: git clone log:\n";
-		foreach my $line (@$clonelog)
+		print "ABICompCheck:";
+		foreach my $line (@$logss)
 		{
-			print $line, "\n";
+			print $line;
 		}
 	}
 }
@@ -392,8 +450,8 @@ sub install
 		  if $verbose;
 
 		my @checkout_log = run_log(qq{git checkout -q "$branch"});
-		print_logs(\@checkout_log);
 		$status = $? >> 8;
+		print_logs(\@checkout_log);
 		if ($status)
 		{
 			die "git checkout failed with status $status";
@@ -417,25 +475,25 @@ sub install
 				  if $verbose;
 				return;
 			}
-
 			_process_commits_list($self, \@two_commit_hashes);
-			return;
 		}
-
-		my @commits = `git rev-list --reverse $last_commit_hash..HEAD`;
-		chomp @commits;
-		if (!@commits)
+		else
 		{
-			print time_str(),
-			  "ABICompCheck: No new commits found since last run. Skipping build.\n"
-			  if $verbose;
-			return;
-		}
+			my @commits = `git rev-list --reverse $last_commit_hash..HEAD`;
+			chomp @commits;
+			if (!@commits)
+			{
+				print time_str(),
+				  "ABICompCheck: No new commits found since last run. Skipping build.\n"
+				  if $verbose;
+				return;
+			}
 
-		print time_str(),
-		  "ABICompCheck: Found new commits since last run. Processing commits:\n"
-		  if $verbose;
-		_process_commits_list($self, @commits);
+			print time_str(),
+			  "ABICompCheck: Found new commits since last run. Processing commits:\n"
+			  if $verbose;
+			_process_commits_list($self, @commits);
+		}
 	}
 	else
 	{
@@ -449,6 +507,30 @@ sub cleanup
 	my $self = shift;
 
 	print time_str(), "cleaning up ", __PACKAGE__, "\n" if $verbose > 1;
+
+	my $head_commit_hash = $self->{head_commit_hash};
+	my $abi_compare_root = $self->{abi_compare_root};
+	if (defined $head_commit_hash && $head_commit_hash ne '')
+	{
+		my $abi_compare_root = $self->{abi_compare_root};
+		my $branch = $self->{pgbranch};
+		my $commit_hash_dir = "$abi_compare_root/$branch";
+		my $last_commit_hash_file = "$commit_hash_dir/last_pg_commit_hash.txt";
+
+		mkdir $commit_hash_dir unless -d $commit_hash_dir;
+
+		open my $fh, '>', $last_commit_hash_file
+		  or die "Cannot open $last_commit_hash_file for write: $!";
+		print $fh "$head_commit_hash\n";
+		close $fh;
+		print time_str(),
+		  "ABICompCheck: Stored commit hash $head_commit_hash in $last_commit_hash_file\n"
+		  if $verbose;
+	}
+	rmdir $abi_compare_root
+	  if -d $abi_compare_root
+	  && $abi_compare_root ne '.'
+	  && $abi_compare_root ne '/';
 	return;
 }
 
