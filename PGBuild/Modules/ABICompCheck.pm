7990e354542d96f96cf20c98434a18b6727104bf
Mankirat Singh <msingh2_be22@thapar.edu>
2025-06-26 01:29:42 +0530

    add fast path comparing logic for multiple commits in lag

--- PGBuild/Modules/ABICompCheck.pm
+++ PGBuild/Modules/ABICompCheck.pm
@@ -52,18 +52,19 @@ sub setup
 		$abi_compare_root = "$buildroot/abicheck";
 	}
 
-	my %binaries_rel_path = $conf->{binaries_rel_path} || (
+	my %binaries_rel_path = $conf->{binaries_rel_path}
+	  || (
 		'postgres' => 'bin/postgres',
 		'ecpg' => 'bin/ecpg',
 		'libpq.so' => 'lib/libpq.so',
-	);
+	  );
 
-	my @abidw_flags_list = $conf->{abidw_flags_list} || (
-		'--drop-undefined-syms', '--no-architecture',
-		'--no-comp-dir-path', '--no-elf-needed',
-		'--no-show-locs', '--type-id-style',
+	my @abidw_flags_list = $conf->{abidw_flags_list}
+	  || (
+		'--drop-undefined-syms', '--no-architecture', '--no-comp-dir-path',
+		'--no-elf-needed', '--no-show-locs', '--type-id-style',
 		'hash',
-	);
+	  );
 
 	mkdir $abi_compare_root
 	  unless -d $abi_compare_root;
@@ -73,8 +74,6 @@ sub setup
 	  unless -d "$abi_compare_root/xmls";
 	mkdir "$abi_compare_root/install"
 	  unless -d "$abi_compare_root/install";
-	mkdir "$abi_compare_root/diffs"
-	  unless -d "$abi_compare_root/diffs";
 
 	my $last_commit_hash_file = "$abi_compare_root/githead.log";
 	my $last_commit_hash;
@@ -136,8 +135,7 @@ sub need_run
 	else
 	{
 		print "running ", qq{git clone -q  "$mirror_base" "$clone_name"}, "\n";
-		@clonelog =
-		  run_log(qq{git clone -q "$mirror_base" "$git_repo_path"});
+		@clonelog = run_log(qq{git clone -q "$mirror_base" "$git_repo_path"});
 		$status = $? >> 8;
 		print_logs(\@clonelog);
 		if ($status)
@@ -146,13 +144,15 @@ sub need_run
 		}
 	}
 
-	my @checkout_log = run_log(qq{git -C "$git_repo_path" checkout -q "$branch"});
+	my @checkout_log =
+	  run_log(qq{git -C "$git_repo_path" checkout -q "$branch"});
 	$status = $? >> 8;
 	print_logs(\@checkout_log);
 	if ($status)
 	{
 		die "git checkout failed with status $status";
 	}
+
 	# my @pull_log = run_log(qq{git -C "$git_repo_path" pull});
 	# $status = $? >> 8;
 	# print_logs(\@pull_log);
@@ -363,17 +363,29 @@ sub _generate_abidw_xml
 	return;
 }
 
-sub _compare_abixml_using_abidiff
+sub _compare_and_log_abi_diff
 {
-	my ($self, $old_commit_hash, $new_commit_hash) = @_;
+	my ($self, $old_commit_hash, $new_commit_hash, $fail_fast) = @_;
+
+	if (!defined $old_commit_hash || !defined $new_commit_hash)
+	{
+		print time_str(),
+		  "Warning: _compare_and_log_abi_diff called with undefined commit hash. Skipping comparison.\n"
+		  if $verbose;
+		return 0;
+	}
+
 	my $abi_compare_root = $self->{abi_compare_root};
 
 	print time_str(),
-	  "Comparing ABI XML for commits $old_commit_hash and $new_commit_hash in ",
-	  __PACKAGE__, "\n"
+	  "Comparing ABI for commits $old_commit_hash and $new_commit_hash\n"
 	  if $verbose;
+
 	my $old_commit_xml_dir = "$abi_compare_root/xmls/$old_commit_hash";
 	my $new_commit_xml_dir = "$abi_compare_root/xmls/$new_commit_hash";
+	my $log_dir = "$abi_compare_root/diffs";
+	mkdir $log_dir unless -d $log_dir;
+	my $diff_found = 0;
 
 	foreach my $key (keys %{ $self->{binaries_rel_path} })
 	{
@@ -382,42 +394,147 @@ sub _compare_abixml_using_abidiff
 
 		if (-e $old_file && -e $new_file)
 		{
-			my $log_dir = "$abi_compare_root/diffs";
-			_log_command_output(
-				$self, "abidiff $old_file $new_file",
-				$log_dir, "$key-$old_commit_hash-$new_commit_hash"
-			);
+			my $log_file =
+			  "$log_dir/$key-$old_commit_hash-$new_commit_hash.log";
+			my @output = run_log("abidiff \"$old_file\" \"$new_file\"");
+			my $exit_status = $? >> 8;
+			if ($exit_status != 0)
+			{
+				$diff_found = 1;
+				open my $fh, '>', $log_file
+				  or warn "could not open $log_file: $!";
+				print $fh @output if $fh;
+				close $fh if $fh;
+				print time_str(),
+				  "ABI difference found for $key. Log: $log_file\n"
+				  if $verbose;
+				return 1 if $fail_fast;
+			}
 		}
-		else
+		elsif (-e $old_file xor -e $new_file)
 		{
+			$diff_found = 1;
 			print time_str(),
-			  "Warning: One of the ABI XML files for $key does not exist. Skipping comparison.\n"
+			  "ABI difference for $key: one file is missing (old: $old_file, new: $new_file). Comparison skipped.\n"
 			  if $verbose;
+			my $log_file =
+			  "$log_dir/$key-$old_commit_hash-$new_commit_hash.log";
+			open my $fh, '>', $log_file
+			  or warn "could not open $log_file: $!";
+			if ($fh)
+			{
+				print $fh "ABI difference: one file is missing.\n";
+				print $fh "Old file: $old_file (exists: "
+				  . (-e $old_file) . ")\n";
+				print $fh "New file: $new_file (exists: "
+				  . (-e $new_file) . ")\n";
+				close $fh;
+			}
+			return 1 if $fail_fast;
 		}
 	}
-	return;
+
+	if (!$diff_found)
+	{
+		print time_str(),
+		  "No ABI differences found between $old_commit_hash and $new_commit_hash\n"
+		  if $verbose;
+	}
+	return $diff_found;
 }
 
 sub _process_commits_list
 {
-	my $self = shift;
-	my $commits_list = shift;
+	my ($self, $commits_list, $initial_prev_commit) = @_;
 	my $scm = $self->{scm};
 
 	print time_str(), "Processing commits list in ", __PACKAGE__, "\n"
 	  if $verbose;
 
-	my $prev_commit;
+	if (defined $initial_prev_commit && @$commits_list > 2)
+	{
+		my $head_commit_hash = $self->{head_commit_hash};
+		print time_str(),
+		  "ABICompCheck: Found more than one new commit, checking for ABI changes between start and end.\n"
+		  if $verbose;
+
+		# First, check if the ABI data for the old commit exists.
+		# If not, we can't do the fast-path comparison.
+		my $can_do_fast_path = 1;
+		my $old_commit_xml_dir =
+		  "$self->{abi_compare_root}/xmls/$initial_prev_commit";
+
+		if (!-d $old_commit_xml_dir)
+		{
+			$can_do_fast_path = 0;
+		}
+		else
+		{
+			foreach my $key (keys %{ $self->{binaries_rel_path} })
+			{
+				my $abi_file = "$old_commit_xml_dir/$key.abi";
+				if (!-e $abi_file)
+				{
+					$can_do_fast_path = 0;
+					last;
+				}
+			}
+		}
+
+		if (!$can_do_fast_path)
+		{
+			print time_str(),
+			  "ABICompCheck: Missing ABI data for $initial_prev_commit. Will process all commits.\n"
+			  if $verbose;
+		}
+		else
+		{
+			# Build and generate XML for the head commit
+			_checkout_commit($self, $head_commit_hash);
+			_configure_make_and_build($self, $head_commit_hash);
+			_generate_abidw_xml($self, $head_commit_hash);
+
+			# Compare with the last known commit
+			my $diff_found =
+			  _compare_and_log_abi_diff($self, $initial_prev_commit,
+				$head_commit_hash, 1);
+
+			if (!$diff_found)
+			{
+				print time_str(),
+				  "ABICompCheck: No ABI difference between $initial_prev_commit and $head_commit_hash. Skipping intermediate builds.\n"
+				  if $verbose;
+				return;
+			}
+			else
+			{
+				print time_str(),
+				  "ABICompCheck: ABI difference found between $initial_prev_commit and $head_commit_hash. Processing all intermediate commits.\n"
+				  if $verbose;
+			}
+		}
+	}
+	my $prev_commit = $initial_prev_commit;
 	my $idx = 0;
 	foreach my $commit (@$commits_list)
 	{
-		_checkout_commit($self, $commit);
-		_configure_make_and_build($self, $commit);
-		_generate_abidw_xml($self, $commit);
+		my $commit_xml_dir = "$self->{abi_compare_root}/xmls/$commit";
+		if (-d $commit_xml_dir)
+		{
+			print time_str(),
+			  "XML for commit $commit already exists, skipping build.\n"
+			  if $verbose;
+		}
+		else
+		{
+			_checkout_commit($self, $commit);
+			_configure_make_and_build($self, $commit);
+			_generate_abidw_xml($self, $commit);
+		}
 
 		if (defined $prev_commit)
 		{
-			_compare_abixml_using_abidiff($self, $prev_commit, $commit);
+			_compare_and_log_abi_diff($self, $prev_commit, $commit, 0);
 		}
 		$prev_commit = $commit;
 		$idx++;
@@ -459,8 +576,7 @@ sub install
 		print time_str(),
 		  "ABICompCheck: No previous commit hash found, comparing last 2 commits only.\n"
 		  if $verbose;
-		my @two_commit_hashes = split /\n/,
-		  `git rev-list --max-count=2 HEAD^`;
+		my @two_commit_hashes = split /\n/, `git rev-list --max-count=2 HEAD`;
 		print "Two commit hashes: @two_commit_hashes\n" if $verbose;
 		if (@two_commit_hashes < 2)
 		{
@@ -473,7 +589,8 @@ sub install
 	}
 	else
 	{
-		my @commits = `git rev-list --reverse $last_commit_hash..$head_commit_hash`;
+		my @commits =
+		  `git rev-list --reverse $last_commit_hash..$head_commit_hash`;
 		chomp @commits;
 		if (!@commits)
 		{
@@ -484,9 +601,10 @@ sub install
 		}
 
 		print time_str(),
-		  "ABICompCheck: Found new commits since last run. Processing " . scalar(@commits) . " commits:\n"
+		  "ABICompCheck: Found new commits since last run. Processing ",
+		  scalar(@commits), " commits:\n"
 		  if $verbose;
-		_process_commits_list($self, \@commits);
+		_process_commits_list($self, \@commits, $last_commit_hash);
 	}
 	return;
 }
